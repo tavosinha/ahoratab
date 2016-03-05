855b47e866665e6083e41370d40fa678c6085917
Gustavo Castillo <tavosinha@gmail.com>
2016-03-05 10:46:06 -0600

    Prueba con repositorios remotos

--- index.php
+++ index.php
@@ -15,7 +15,15 @@
   			<div class="col-md-12">  				
     			<div class="jumbotron">
             <h1>Jumbotron!</h1>
-            <button class="btn btn-success">Haz click</button>    
+            <button class="btn btn-success">Haz click</button>
+            <p>
+            	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
           </div>
   			</div>
   		</div>
